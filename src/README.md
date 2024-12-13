17a15e200c7fe0a3c6a01833b05950bbfb48bc6d
Fred Morrison <frederick.morrison@outlook.com>
2024-12-13 13:09:45 -0700

    add read me

---
+++ src/README.md
@@ -0,0 +1,18 @@
+# AP CS A - Sound without Silence FRQ
+An example taken from the AP CSA exam
+## Tools Used
+
+| Tool     |  Version |
+|:---------|---------:|
+| Java     | 23.0.1.0 |
+| IntelliJ | 2024.3.0 |
+| VSCode   |   1.96.0 |
+
+## Change History
+
+| Date       | Description      |
+|:-----------|:-----------------|
+| 2024-12-13 | Initial creation |
+
+## References
+
--- src/Sound.java
+++ src/Sound.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sound {
 
 
@@ -65,5 +67,19 @@ public class Sound {
             }
         }
         return n;
+    }
+
+    public static void main(String[] args) {
+        Sound s = new Sound();
+
+        System.out.println(
+                "The original array of samples is " + Arrays.toString(s.getSamples()));
+
+        s.trimSilenceFromBeginning();
+        System.out.println(
+                "The new array of samples is " + Arrays.toString(s.getSamples()));
+        System.out.println(
+                "The length of the new array should be 12 and is "
+                        + s.getSamples().length);
     }
 }
